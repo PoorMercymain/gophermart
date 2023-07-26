5f38e062ea844a5778da4605616c5c6cd0f4d397
PoorMercymain <pinegin17@bk.ru>
2023-07-26 19:48:03 +0300

    made fixes for getting float accrual and withdrawal

--- internal/domain/accrualorder.go
+++ internal/domain/accrualorder.go
@@ -1,7 +1,22 @@
 package domain
 
+import "encoding/json"
+
 type AccrualOrder struct {
 	Order   string `json:"order"`
 	Status  string `json:"status"`
-	Accrual string `json:"accrual"`
+	Accrual accrual `json:"accrual"`
+}
+
+type accrual int
+
+func (a *accrual) UnmarshalJSON(data []byte) error {
+	var accrualFloat float64
+
+	if err := json.Unmarshal(data, &accrualFloat); err != nil {
+		return err
+	}
+
+	*a = accrual(int(accrualFloat * 100))
+	return nil
 }
